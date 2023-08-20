26966a3881e6cb8482ca977bd1919f34727c0787
kg2784 <kify_job-study@yahoo.co.jp>
2023-08-21 00:42:43 +0900

    fix

--- src/components/CopyRight/index.tsx
+++ src/components/CopyRight/index.tsx
@@ -3,7 +3,7 @@ import styles from './style.module.scss'
 const CopyRight = () => {
   return (
     <div className={styles.CopyRight}>
-    <small>&copy; 2023 kie funahashi</small>
+    <small>&copy; 2023 k2784</small>
     </div>
   )
 }
