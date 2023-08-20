c045728ecac61abec7aa617106e329e00e1be616
kg2784 <kify_job-study@yahoo.co.jp>
2023-08-20 21:40:00 +0900

    fix HOME title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.Home}>
       <div className={styles.Home__mainTextArea}>
-        <p className={styles.Home__mainText}>k2784 is</p>
+        <h1 className={styles.Home__mainText}>k2784 is</h1>
         <div className={styles.Home__line} />
         {/* <label>
           <input type="text" />
