6c98d857f16d55497a8fc4f1e3203af5c7f972c0
kg2784 <kify_job-study@yahoo.co.jp>
2023-08-21 00:32:24 +0900

    note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import styles from './page.module.scss'
 
+// TODO: ペンで絵をかけるように
+// TODO: ペンの色や選択した色を背景に適用する
+// TODO: そのためのカラーパレット
 export default function Home() {
   return (
     <div className={styles.Home}>
       <div className={styles.Home__mainTextArea}>
         <h1 className={styles.Home__mainText}>k2784 is</h1>
         <div className={styles.Home__line} />
+        {/* TODO: 入力したものを下線の下に表示出来るようにする */}
         {/* <label>
           <input type="text" />
         </label> */}
